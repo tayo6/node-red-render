db566d21b1e2e08cc8a86d07762e6a3014c5ec35
Tayo <92905932+tayo6@users.noreply.github.com>
2026-08-31 14:21:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,4 @@
 module.exports = {
     uiHost: "0.0.0.0",
-    uiPort: Number(process.env.PORT) || 1880,
-
-    adminAuth: {
-        type: "credentials",
-        users: [
-            {
-                username: process.env.NODE_RED_USERNAME,
-                password: process.env.NODE_RED_PASSWORD_HASH,
-                permissions: "*"
-            }
-        ]
-    }
+    uiPort: Number(process.env.PORT) || 1880
 };
