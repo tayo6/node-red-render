2c98d4ed5d6ba40649aac40ad4c323b26bb54c75
Tayo <92905932+tayo6@users.noreply.github.com>
2026-08-31 14:21:22 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PASSWORD_HASH=$(printf '%s\n' "$NODE_RED_PASSWORD" | node-red admin hash-pw | tail -n 1)
-
-export NODE_RED_PASSWORD_HASH="$PASSWORD_HASH"
-
-exec npm start -- --settings /data/settings.js
